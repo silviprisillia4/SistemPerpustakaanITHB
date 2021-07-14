7485ab0b841c9590291321b9322e3795c52fc6d5
Feli <G512LI@LAPTOP-IF3UEPDT>
2021-07-15 06:56:14 +0700

    membetulkan login handler

--- src/controller/LoginHandler.java
+++ src/controller/LoginHandler.java
@@ -6,13 +6,15 @@ public class LoginHandler {
     
     public LoginHandler(String email, String branch) {
         Controller c = new Controller();
+        
         int idBranch = c.getBranchIDByCity(branch);
         c.getAUser(email, idBranch);
+        
         User user = new model.UserManager().getInstance().getUser();
-        if (user.getType() == UserTypeEnum.ADMIN) {
+        if (user instanceof Admin) {
             new view.OutputInfo().welcomeToMenuAdmin();
             new AdminMenu();
-        } else if (user.getType() == UserTypeEnum.MEMBER) {
+        } else if (user instanceof Member) {
             new MemberMenu();
         } else {
             new view.OutputInfo().welcomeToOwnerMenu();
